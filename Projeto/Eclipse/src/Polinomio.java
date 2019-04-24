3550771e6dab3650467d2dabb9a91f0feef7656a
Jonathas Leandro <jonathasbent@gmail.com>
2019-04-24 20:58:33 -0300

    irrelevante

--- Projeto/Eclipse/src/Polinomio.java
+++ Projeto/Eclipse/src/Polinomio.java
@@ -4,47 +4,62 @@ public class Polinomio {
 	private String polStr;
 	private double multiplicador;
 	private double potencia;
-	private boolean pot;
+	private boolean hasPotency;
+	private boolean noVariable;
 
 	public Polinomio(String polinomio) {
 		this.polStr = polinomio;
 
 		//Se tem potencia
 		if(polStr.contains("x^")) {
-			this.pot = false;
+			this.hasPotency = false;
 			StringTokenizer str = new StringTokenizer(polStr,"x^");
 
 			if(!polStr.startsWith("x^")) {
 				this.multiplicador = Double.parseDouble(str.nextToken());
 				this.potencia = Double.parseDouble(str.nextToken());
 			}else {
-				this.multiplicador = 0;
+				this.multiplicador = 1;
 				this.potencia = Double.parseDouble(str.nextToken());
 			}
 		}else {
 			//Se nao tem potencia
 			if(!polStr.contains("x^")) {
-				this.pot = true;
-				this.multiplicador = Double.parseDouble(polinomio);
+				this.hasPotency = true;
+				if(polStr.contains("x")) {
+					StringTokenizer str1 = new StringTokenizer(polStr,"x");
+					this.multiplicador = Double.parseDouble(str1.nextToken());
+					this.potencia = 1;
+				}else {
+					this.multiplicador = Double.parseDouble(polStr);
+					this.potencia = 0; //0 para explicitar que não tem variavel
+				}
 			}
 		}
 	}
 
 	public String toString() {
-		if(pot)
+		if(hasPotency)
 			return String.valueOf(multiplicador) + "x";
 		else
 			return String.valueOf(multiplicador) + "x^" + String.valueOf(potencia);
 	}
-	
+
 	public String univariateString() {
 		StringBuilder sb = new StringBuilder();
-		if(pot) {
+		if(hasPotency) {
 			sb.append(multiplicador).append(" * Math.pow(x, 1)");
 		}
 		else {
 			sb.append(multiplicador).append(" * Math.pow(x, potencia)");
 		}
 		return sb.toString();
+	}
+
+	public double getResultado(Double x) {
+		if(this.potencia==0)
+			return multiplicador;
+		else
+			return multiplicador * Math.pow(x, potencia);
 	}
 }
